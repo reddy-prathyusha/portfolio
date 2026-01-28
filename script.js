c2005b3fcd005cf882690f3778ad5614166cc3a3
reddy-prathyusha <prathyushanagire@gmail.com>
2026-01-28 09:36:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.style.opacity = "1";
+      el.style.transform = "translateY(0)";
+    }
+  });
+});
